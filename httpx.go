2593b3f8b03175b9ab7ea5ff528472c6547cd025
t0k4r <mi03to08@gmail.com>
2024-03-29 00:23:16 +0100

    update

--- httpx.go
+++ httpx.go
@@ -8,96 +8,94 @@ import (
 	"net/http"
 )
 
-type ResponseWriter struct {
-	http.ResponseWriter
-	sendErr bool
-}
-
-func (w ResponseWriter) Error(code int, err error) error {
+func Error(w http.ResponseWriter, err error, code int) error {
 	http.Error(w, err.Error(), code)
-	w.sendErr = false
-	return err
-}
-
-func (w ResponseWriter) NoContent(code int) error {
-	w.WriteHeader(code)
 	return nil
 }
 
-func (w ResponseWriter) SqlcJson(code int, v any, err error) error {
+func Empty(w http.ResponseWriter, code int) error {
+	w.WriteHeader(code)
+	return nil
+}
+func Json(w http.ResponseWriter, v any, code int) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	return json.NewEncoder(w).Encode(v)
+}
+func JsonMany[T any](w http.ResponseWriter, v []T, code int) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(code)
+	switch len(v) {
+	case 0:
+		_, err := fmt.Fprint(w, "[]")
+		return err
+	default:
+		return json.NewEncoder(w).Encode(v)
+	}
+}
+func JsonSql(w http.ResponseWriter, v any, err error, code int) error {
 	switch err {
 	case nil:
-		if v == nil {
-			w.ResponseWriter.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(code)
-			_, err := fmt.Fprint(w, "[]")
-			return err
-		}
-		return w.Json(code, v)
+		return Json(w, v, code)
 	case sql.ErrNoRows:
-		return w.NoContent(404)
+		return Empty(w, 404)
 	default:
-		return w.Error(500, err)
+		return err
+	}
+}
+func JsonSqlMany[T any](w http.ResponseWriter, v []T, err error, code int) error {
+	switch err {
+	case nil:
+		return JsonMany(w, v, code)
+	case sql.ErrNoRows:
+		return Empty(w, 404)
+	default:
+		return err
 	}
 }
 
-func (w ResponseWriter) Json(code int, v any) error {
-	w.ResponseWriter.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	return json.NewEncoder(w.ResponseWriter).Encode(v)
-}
+type HandlerFunc func(http.ResponseWriter, *http.Request) error
 
-type HandlerFunc func(*ResponseWriter, *http.Request) error
+type MiddlewareFunc func(http.Handler) http.Handler
 
 func (f HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	wx := ResponseWriter{
-		ResponseWriter: w,
-		sendErr:        true,
-	}
-	err := f(&wx, r)
+	err := f(w, r)
 	if err != nil {
-		if wx.sendErr {
-			http.Error(wx, err.Error(), 500)
-		}
-		slog.Error(err.Error(), "url", r.URL.Path)
+		slog.Error(err.Error(), "type", "error", "parh", r.URL.Path)
+		http.Error(w, err.Error(), 500)
 	}
 }
-
-type MiddlewareFunc func(next http.Handler) http.Handler
 
 type ServeMux struct {
 	*http.ServeMux
+	middlewares []MiddlewareFunc
 }
 
 func NewServeMux() *ServeMux {
 	return &ServeMux{
-		ServeMux: &http.ServeMux{},
+		ServeMux:    &http.ServeMux{},
+		middlewares: []MiddlewareFunc{},
 	}
 }
 
-func (mux *ServeMux) HandleFunc(pattern string, handler HandlerFunc, middlewares ...MiddlewareFunc) {
-	switch len(middlewares) {
-	case 0:
-		mux.Handle(pattern, handler)
-	default:
-		midhandler := middlewares[0](handler)
-		for i, mid := range middlewares {
-			if i == 0 {
-				continue
-			}
-			midhandler = mid(midhandler)
-		}
-		mux.Handle(pattern, midhandler)
+func (mux *ServeMux) Use(middlewares ...MiddlewareFunc) {
+	mux.middlewares = append(mux.middlewares, middlewares...)
+}
+func (mux *ServeMux) wrap(handler http.Handler, middlewares ...MiddlewareFunc) http.Handler {
+	for _, mid := range append(mux.middlewares, middlewares...) {
+		handler = mid(handler)
 	}
+	return handler
 }
 
-func (mux *ServeMux) Use(middlewares ...MiddlewareFunc) http.Handler {
-	midhandler := middlewares[0](mux.ServeMux)
-	for i, mid := range middlewares {
-		if i == 0 {
-			continue
-		}
-		midhandler = mid(midhandler)
-	}
-	return midhandler
+func (mux *ServeMux) Handle(pattern string, handler http.Handler, middlewares ...MiddlewareFunc) {
+	mux.ServeMux.Handle(pattern, mux.wrap(handler))
+}
+func (mux *ServeMux) HandleFunc(pattern string, handler http.HandlerFunc, middlewares ...MiddlewareFunc) {
+	mux.ServeMux.Handle(pattern, mux.wrap(handler))
+}
+
+func (mux *ServeMux) Handlex(pattern string, handler HandlerFunc, middlewares ...MiddlewareFunc) {
+	mux.ServeMux.Handle(pattern, mux.wrap(handler))
 }
